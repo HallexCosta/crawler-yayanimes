707c7cf9ce3ae6dcc0f13b40eda755b186f5c757
HallexCosta <hallex.costa@hotmail.com>
2020-10-27 13:51:59 -0300

    Adding function createLogFile

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,6 +6,11 @@ type JSONFile = {
   data: [] | {}
 }
 
+type EmptyFile = {
+  filename: string
+  contentText: string
+}
+
 export class Utils {
   static createJSONFile(json: JSONFile) {
     const { filename, data } = json
@@ -14,6 +19,19 @@ export class Utils {
     console.log(`> Save directory file: src/__generated__/${filename}.${ext}`)
 
     fs.writeFile(path.join(__dirname, '../', '__generated__', `${filename}.${ext}`), JSON.stringify(data, null, 2), err => {
+      if (err) throw new Error('something went wrong.')
+      
+      console.log(`> File ${filename}.${ext} was created successfully!`)
+    })
+  }
+
+  static createLogFile(file: EmptyFile) {
+    const { filename, contentText } = file
+    const ext = 'log' 
+
+    console.log(`> Save directory file: src/__generated__/${filename}.${ext}`)
+
+    fs.writeFile(path.join(__dirname, '../', '__generated__', `${filename}.${ext}`), contentText, err => {
       if (err) throw new Error('something went wrong.')
       
       console.log(`> File ${filename}.${ext} was created successfully!`)
