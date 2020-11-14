ca699984b8bfa54e2f752e121e8dfc7baf84b1d0
HallexCosta <hallex.costa@hotmail.com>
2020-11-14 19:23:44 -0300

    Remove executablePath for opera

--- puppeteer.config.ts
+++ puppeteer.config.ts
@@ -4,7 +4,6 @@ export const base_url = 'https://yayanimes.net'
 export const launch = {
   headless: false,
   executablePath: path.join('/', 'mnt', 'c', 'chrome-win', 'chrome.exe'),
-  // executablePath: path.join('/', 'mnt', 'c', 'Users', 'halle', 'AppData', 'Local', 'Programs', 'Opera', 'launcher.exe'),
   devtools: true,
   args:['--no-sandbox'],
   defaultViewport: { width: 1600, height: 800 }
