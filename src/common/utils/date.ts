db9d2856e39f830771828b3af9ff814be8a8a1b2
HallexCosta <hallex.costa@hotmail.com>
2020-12-07 19:56:05 -0300

    Add utils for date

--- src/common/utils/date.ts
+++ src/common/utils/date.ts
@@ -1,62 +1,55 @@
-const monthsAbbreviation= [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sep',
-  'Oct',
-  'Nov',
-  'Dec'
-]
-const monthsComplete= [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December'
-]
 
-export function getCurrentDate(abbreviateMonth: boolean = false) {
+
+export function getCurrentDate(showNameMonth: boolean = false, abbreviateMonth: boolean = false) {
+  const monthsAbbreviation= [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ]
+  const monthsComplete= [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]
+  
   const today = new Date()
   
   const currentYear = today.getFullYear()
-  const currentMonth = 
-    abbreviateMonth
+
+  const currentMonth: string | number = showNameMonth
+    ? abbreviateMonth
     ? monthsAbbreviation[today.getMonth()]
     : monthsComplete[today.getMonth()]
+    : today.getMonth()
+
 
   const currentDay = today.getDate();
-
-  const currentHours = today.getHours()
-  const currentMinutes = today.getMinutes()
-  const currentSeconds = today.getSeconds()
 
   const currentDate = `${currentYear}-${currentMonth}-${currentDay}`
 
   return currentDate
 }
 
-export function getCurrentTime(abbreviateMonth: boolean = false) {
+export function getCurrentTime() {
   const today = new Date()
-  
-  const currentYear = today.getFullYear()
-  const currentMonth = 
-    abbreviateMonth
-    ? monthsAbbreviation[today.getMonth()]
-    : monthsComplete[today.getMonth()]
-
-  const currentDay = today.getDate();
 
   const currentHours = today.getHours()
   const currentMinutes = today.getMinutes()
